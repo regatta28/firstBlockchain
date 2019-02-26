358f2b8dd852ce823bd4c84b0ef4a8ab2cf5ef7a
Rebecca Stevens <rsbjshan@gmail.com>
2019-02-26 22:38:14 +0000

    Values for keys of add_transaction post in json format with single quotes but in the json file needs double quotes

--- createCryptocurrency/rebcoin_port_5002.py
+++ createCryptocurrency/rebcoin_port_5002.py
@@ -165,10 +165,10 @@ def is_valid():
 
 def add_transaction():
     json = request.get_json()
-    transaction_keys = ["sender", "receiver", "amount"]
+    transaction_keys = ['sender', 'receiver', 'amount']
     if not all (key in json for key in transaction_keys):
         return 'Some elements of the transaction are missing.', 400
-    index = blockchain.add_transaction(json["sender"], json["receiver"], json["amount"])
+    index = blockchain.add_transaction(json['sender'], json['receiver'], json['amount'])
     response = {'message': f'This transaction will be added to block {index}'}
     return jsonify(response), 201
     
