fdbe100f1811d59f7c909485da298671a1abca0b
Rebecca Stevens <rsbjshan@gmail.com>
2019-02-26 10:32:49 +0000

    Added replaced chain get request. Returns a response of replaced chain if in fact the chain has been replaced using the replace_chain method to check if it has been replaced, and replaces a message and actual chain if the chain stays the same because it is the longest.

--- createCryptocurrency/Rebcoin.py
+++ createCryptocurrency/Rebcoin.py
@@ -185,6 +185,18 @@ def connect_node():
                 'total_nodes': list(blockchain.nodes)}
     return jsonify(response), 201
 
+# Replacing the chain by the longest chain if needed
+    
+@app.route('/replace_chain', methods = ['GET'])
+def replace_chain():
+    is_chain_replaced = blockchain.replace_chain()
+    if is_chain_replaced:
+        response = {'message': 'Node has different chains so the chain was replaced by the longest chain',
+                    'new_chain': blockchain.chain}
+    else:
+        response = {'message': 'All good. The chain is the largest one.',
+                    'actual_chain': blockchain.chain}
+    return jsonify(response), 200
 
 # Running the app
     
