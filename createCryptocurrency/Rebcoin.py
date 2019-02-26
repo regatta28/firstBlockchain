e778cc86d2682037d82f5f878316aa38ff8b4346
Rebecca Stevens <rsbjshan@gmail.com>
2019-02-26 10:18:17 +0000

    Added post request for connecting a node to the network. The node argument for the add_node function represents the address of the new node.

--- createCryptocurrency/Rebcoin.py
+++ createCryptocurrency/Rebcoin.py
@@ -170,6 +170,20 @@ def add_transaction():
 
 # PART 3 ----     Decentralising our Blockchain 
 
+# Connecting the new nodes
+    
+@app.route('/connect_node', methods = ['POST'])
+
+def connect_node():
+    json = request.get_json()
+    nodes = json.get('nodes')
+    if nodes is None:
+        return "No Node", 400
+    for node in nodes:
+        blockchain.add_node(node)
+    response = {'message': 'All the nodes are now connected. The Rebcoin Blockchain now contains the following nodes: ',
+                'total_nodes': list(blockchain.nodes)}
+    return jsonify(response), 201
 
 
 # Running the app
